1a977cdd6c2aa0b814960dfdb595976ca2928d07
cloudluis22 <cloudluis22@hotmail.com>
2022-03-25 18:19:44 -0600

    Preparando verificacion de datos en loginController.js

--- src/controllers/loginController.js
+++ src/controllers/loginController.js
@@ -9,4 +9,14 @@ controller.show = (req, res) => {
   res.render('login');
 };
 
+controller.login = (req, res) => {
+  const data = req.body;
+  req.getConnection((err, conn) => {
+    conn.query('SELECT * FROM usuarios WHERE', (err, rows) => {
+      console.log(rows);
+      res.send('sarahi 2');
+    });
+  });
+};
+
 module.exports = controller;
--- src/routes/login.js
+++ src/routes/login.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const loginController = require('../controllers/loginController');
 
 router.get('/', loginController.show);
+router.post('/iniciar', loginController.login);
 
 module.exports = router;
